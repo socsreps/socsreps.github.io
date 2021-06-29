6f79ef20e0148cfa13ae05baad3cc72de5667188
William Oldham <wegg7250@gmail.com>
2021-06-29 11:45:57 +0100

    Feedback page start and tab system JS

--- js/common.js
+++ js/common.js
@@ -2,7 +2,28 @@ function showVerticalNav() {
     document.getElementById("vertical-nav").classList.toggle("hidden");
 }
 
-document.querySelectorAll("*[data-tooltip]").forEach(node => {
+// Src: https://www.javascripttutorial.net/javascript-dom/javascript-siblings/
+function getSiblings(e) {
+    // for collecting siblings
+    let siblings = []; 
+    // if no parent, return no sibling
+    if(!e.parentNode) {
+        return siblings;
+    }
+    // first child of the parent node
+    let sibling  = e.parentNode.firstChild;
+    
+    // collecting siblings
+    while (sibling) {
+        if (sibling.nodeType === 1 && sibling !== e) {
+            siblings.push(sibling);
+        }
+        sibling = sibling.nextSibling;
+    }
+    return siblings;
+};
+
+document.querySelectorAll("*[data-tab-container][data-tab-target]").forEach(node => {
     const el = document.createElement("span")
     el.innerText = node.getAttribute('data-tooltip');
     el.classList = "tooltip"
@@ -12,5 +33,25 @@ document.querySelectorAll("*[data-tooltip]").forEach(node => {
     })
     node.addEventListener('mouseleave', (e) => {
         e.target.classList.remove('tooltip-open')
+    })
+})
+
+document.querySelectorAll("*[data-tab-container]").forEach(node => {
+    const tabContainerId = node.getAttribute("data-tab-container")
+    const tabTargetId = node.getAttribute("data-tab-target")
+    node.addEventListener("click", (e) => {
+        console.log(tabContainerId, tabTargetId)
+        const container = document.getElementById(tabContainerId);
+        e.target.classList.replace("inactive-tab", "active-tab")
+        getSiblings(e.target).forEach(sibling => sibling.classList.replace("active-tab", "inactive-tab"))
+        if (container) {
+            Array.from(container.children).forEach((targetElement) => {
+                if (targetElement.id === tabTargetId) {
+                    targetElement.classList.remove("hidden")
+                } else {
+                    targetElement.classList.add("hidden")
+                }
+            })
+        }
     })
 })
